10f6cba518e5f27cfd4b57069a4d19422b0b4af0
Denis Kulik <kulikdenis1996x@gmail.com>
2023-03-09 13:33:14 +0700

    refactor Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,34 +1,35 @@
 import { useState } from 'react';
 
 export default function Login() {
-    const [ username, setUsername ] = useState('');
-    const [ password, setPassword ] = useState('');
+    const [ data, setData ] = useState({ username: '', password: '' });
 
     function handleFormSubmit(e) {
         e.preventDefault();
 
-        const userData = {
-            username,
-            password,
-        };
-        console.log(userData);
+        console.log(data);
 
-        e.target.username.value = '';
-        e.target.password.value = '';
+        setData({ username: '', password: '' });
     }
+
+    function handleInputChange(text, name) {
+        setData({ ...data, [name]: text });
+    }
+
 
     return (
         <>
             <h1>Login Form</h1>
             <form onSubmit={handleFormSubmit}>
                 <label>Username:
-                    <input type="text" value={username}
-                           onChange={(e) => setUsername(e.target.value)}
+                    <input type="text" value={data.username}
+                           onChange={(e) => handleInputChange(e.target.value,
+                               'username')}
                     />
                 </label>
                 <label>Password:
-                    <input type="password" value={password}
-                           onChange={(e) => setPassword(e.target.value)}
+                    <input type="password" value={data.password}
+                           onChange={(e) => handleInputChange(e.target.value,
+                               'password')}
                     />
                 </label>
                 <button type="submit">Login</button>
