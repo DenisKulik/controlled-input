b390f108b89def37f9548c401bb537f90e4589bb
Denis Kulik <kulikdenis1996x@gmail.com>
2023-03-09 13:10:37 +0700

    add controlled inputs

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,15 @@
+import { useState } from 'react';
+
 export default function Login() {
+    const [ username, setUsername ] = useState('');
+    const [ password, setPassword ] = useState('');
+
     function handleFormSubmit(e) {
         e.preventDefault();
 
         const userData = {
-            username: e.target.username.value,
-            password: e.target.password.value
+            username,
+            password,
         };
         console.log(userData);
 
@@ -17,10 +22,14 @@ export default function Login() {
             <h1>Login Form</h1>
             <form onSubmit={handleFormSubmit}>
                 <label>Username:
-                    <input type="text" name="username"/>
+                    <input type="text" value={username}
+                           onChange={(e) => setUsername(e.target.value)}
+                    />
                 </label>
                 <label>Password:
-                    <input type="password" name="password"/>
+                    <input type="password" value={password}
+                           onChange={(e) => setPassword(e.target.value)}
+                    />
                 </label>
                 <button type="submit">Login</button>
             </form>
